c6c2c298ac2dc593984745f1e891f50fe1be6358
Nicolas <nicopua7@gmail.com>
2023-07-12 21:02:55 -0300

    correcciones básicas

--- images-gallery/pages/cards/index.tsx
+++ images-gallery/pages/cards/index.tsx
@@ -3,10 +3,10 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function AboutMe() {
+export default function Cards() {
   return (
     <div>
-      <h1>Esto es el About Meeee</h1>
+        <h1>Holaaa</h1>
     </div>
   )
 }
