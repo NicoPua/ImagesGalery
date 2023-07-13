661ac1c16f26f2dc050c5b5801990ccb34d2f680
Nicolas <nicopua7@gmail.com>
2023-07-13 15:11:49 -0300

    Creando Layout

--- images-gallery/components/Layout.tsx
+++ images-gallery/components/Layout.tsx
@@ -1,7 +1,29 @@
-const Layout = () => {
+import React from "react";
+
+const Layout = ({ children, title, description}) => {
     return(
         <div>
-            
+            <nav>
+                <p>Hola</p>
+                <p>Hola</p>
+                <p>Hola</p>
+            </nav>
+
+            <main>{children}</main>
+
+            <footer>
+                <p>Hola</p>
+                <p>Hola</p>
+                <p>Hola</p>
+
+                <p>Hola</p>
+                <p>Hola</p>
+                <p>Hola</p>
+
+                <p>Hola</p>
+                <p>Hola</p>
+                <p>Hola</p>
+            </footer>
         </div>
     )
 }
