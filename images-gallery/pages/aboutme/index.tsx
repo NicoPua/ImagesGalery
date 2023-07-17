e3e44e6761c9abe264731f3bc6b2320595ae2074
Nicolas <nicopua7@gmail.com>
2023-07-17 19:37:45 -0300

    Created pages, Home, About y Cards

--- images-gallery/pages/aboutme/index.tsx
+++ images-gallery/pages/aboutme/index.tsx
@@ -6,8 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function AboutMe() {
   return (
-    <Layout title='About Me' description='Información acerca del creador de esta app.'>
-      <h1>Esto es el About Meeee</h1>
+    <Layout title='About Me | Images-Gallery' description='Información acerca del creador de esta app.'>
+      <div className='pt-10 bg-red-400 w-full h-screen flex items-center'>
+        <h1>Desarrollado por: Gonzalo Nicolás Pua</h1>
+        <h2>Full Stack Web Developer</h2>
+        <p>Esta aplicación fue desarrollada con tecnologías tales como: TypeScript, React JS, Next JS, Tailwind CSS, MongoDB & Mongoose. </p>
+      </div>
     </Layout>
   )
 }
--- images-gallery/pages/cards/index.tsx
+++ images-gallery/pages/cards/index.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
+import Layout from '@/components/Layout'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Cards() {
   return (
-    <div>
-        <h1>Holaaa</h1>
-    </div>
+    <Layout title='Images' description='All Images'>
+      <h1>Holaaa</h1>
+    </Layout>
   )
 }
