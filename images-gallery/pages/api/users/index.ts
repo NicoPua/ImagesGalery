36bcf703552963edf27a71dcc2b8cfbc36b0f8d1
Nicolas <nicopua7@gmail.com>
2023-07-25 20:24:56 -0300

    endpoint users finished

--- images-gallery/pages/api/users/index.ts
+++ images-gallery/pages/api/users/index.ts
@@ -1,9 +1,16 @@
 import { dbConnect, dbDisconnect } from "@/utils/mongoose";
+const User = require('../../../models/User')
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   error?: any;
+}
+
+interface queryObj {
+  deleted: object,
+  active: object,
+  name?: object
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -14,13 +21,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   switch (method) {
     case "GET":
       try {
-        let { name, password, birthdate, profilepic, age} = query;
-        await dbDisconnect();
-        return res.status(200).json({error:"among us"})
+        let { name } = query;
+        const queryOptions: queryObj = {
+          deleted: { $ne: true },
+          active: { $ne: false },
+        };
+
+        if (name) {
+          queryOptions.name = { $regex: `${name}`, $options: "i" };
+        }
+
+        const response = await User.find(queryOptions)
+
+        if (response.length === 0) {
+          await dbDisconnect();
+          return res.status(404).json({ error: `No se encontraron usuarios con el nombre '${name}'.`,});
+        } else {
+          await dbDisconnect();
+          return res.status(200).json(response);
+        }
       } catch (error: any) {
         console.log(error);
         await dbDisconnect();
         return res.status(400).json({ error: error.message });
+      }
+
+    case 'POST':
+      try {
+        let { name, password, birthdate, profilepic, age} = body;
+      } catch (error) {
+        
       }
     break;
 
