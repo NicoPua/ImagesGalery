b48497c578ef41313fc84f079b9074e79f600477
Nicolas <nicopua7@gmail.com>
2023-07-27 21:08:44 -0300

    aux functions folder created

---
+++ images-gallery/aux-functions/validationUserData.ts
@@ -0,0 +1,40 @@
+import { userData } from "@/pages/api/users"
+
+interface errorInfo{
+    message: string,
+}
+const validationUserData = (user: userData) => {
+    const error: errorInfo = {
+        message: ""
+    }
+
+    if(user.firstname.trim().length === 0){
+        error.message = "El nombre es requerido."
+    }else if(!/^[A-Za-z]+$/.test(user.firstname)){
+        error.message = "El nombre sólo debe contener letras."
+    }else if(user.firstname.trim().length >= 18 || user.firstname.trim().length < 2){
+        error.message = "El nombre debe tener entre 18 a 1 características."
+    }
+
+    if(user.lastname.trim().length === 0){
+        error.message = "El apellido es requerido."
+    }else if(!/^[A-Za-z]+$/.test(user.lastname)){
+        error.message = "El apellido sólo debe contener letras."
+    }else if(user.lastname.trim().length >= 18 || user.lastname.trim().length < 2){
+        error.message = "El apellido debe tener entre 18 a 1 características."
+    }
+
+    if(user.name.trim().length === 0){
+        error.message = "El nombre de usuario es requerido."
+    }else if(user.name.trim().length >= 15 || user.name.trim().length <= 6){
+        error.message = "El nombre de usuario debe tener entre 6 a 15 caracteres."
+    }
+
+    if(user.birthdate.length === 0){
+        error.message = "The birthdate is required."
+    }
+
+    return error.message;
+}
+
+export default validationUserData;
