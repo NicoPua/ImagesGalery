d5e31aadeecff6f1b627aec01f3efb9200a3530e
Nicolas <nicopua7@gmail.com>
2023-08-08 01:08:20 -0300

    validation POST photo function finished

--- images-gallery/aux-functions/cleanPostData.ts
+++ images-gallery/aux-functions/cleanPostData.ts
@@ -4,13 +4,13 @@ import { errorInfo } from "@/aux-interfaces/auxLittleInterfaces";
 const cleanPostData = ( { description, location, profilepic } : BodyInformation ) =>{
     const regexWithoutNumbers : RegExp = /^[^0-9]*$/;
     const regexOnlyLetters : RegExp = /^[a-zA-Z,.\s]+$/;
-    const regexOnlyURL : RegExp = /^(https?:\/\/)?[a-zA-Z0-9\-._~:/?#\[\]@!$&'()*+,;=]+$/i;
+    const regexOnlyURL : RegExp = /^(ftp|http|https):\/\/[^ "]+$/
 
     const error : errorInfo = { message: "" };
 
     if(description.length === 0){
         error.message = "La descripción es requerida.";
-    }else if(regexWithoutNumbers.test(description)){
+    }else if(!regexWithoutNumbers.test(description)){
         error.message = "La descripción no debe contener números.";
     }else if(description.length <= 5 || description.length >= 100){
         error.message = "La descripción debe contener entre 5 a 100 caracteres.";
@@ -18,13 +18,13 @@ const cleanPostData = ( { description, location, profilepic } : BodyInformation 
 
     if(location.length === 0){
         error.message = "La location es requerida.";
-    }else if(regexOnlyLetters.test(location)){
+    }else if(!regexOnlyLetters.test(location)){
         error.message = "La location debe contener sólo texto.";
     }else if(description.length <= 2 || description.length >= 50){
         error.message = "La location debe contener entre 2 a 50 caracteres.";
     }
 
-    if(regexOnlyURL.test(profilepic)){
+    if(profilepic && !regexOnlyURL.test(profilepic)){
         error.message = "La imagen debe estar en formato URL.";
     }
 
