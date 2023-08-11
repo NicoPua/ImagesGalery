185bc9ea2311fae2f21af97acf83e2af9f98f736
Nicolas <nicopua7@gmail.com>
2023-08-11 17:10:37 -0300

    new PhotoData interface created

--- images-gallery/aux-interfaces/auxLittleInterfaces.ts
+++ images-gallery/aux-interfaces/auxLittleInterfaces.ts
@@ -1,3 +1,31 @@
 export interface errorInfo{
     message: string,
 }
+//------------------------------------------------------------------------
+export interface SinglePhotoDataAPI{
+    id: string, 
+    uploaded_on: string,
+    description: string,
+    urls: UrlsPhotoInterface,
+    links: string, 
+    likes: number,
+    user_profile: UserInterface,
+    location: object,
+    views: number,
+    downloads: number
+}
+
+interface UrlsPhotoInterface{
+    full_resolution: string,
+    low_resolution: string,
+    download: string,
+}
+
+interface UserInterface{
+    id: string, 
+    username: string,
+    name: string, 
+    links: object,
+    profile_image: string,
+    social: object
+}
