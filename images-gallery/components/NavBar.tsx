ea95c5d0e83dc39571261dcf13ed11774cf62993
Nicolas <nicopua7@gmail.com>
2023-08-15 20:10:40 -0300

    component Navbar correction

--- images-gallery/components/NavBar.tsx
+++ images-gallery/components/NavBar.tsx
@@ -1,6 +1,6 @@
 const NavBar = () =>{
     return (
-        <nav className="w-full h-10 flex flex-row justify-around items-center shadow-xl bg-gray-900 text-gray-100">
+        <nav className="inset-0 z-10 w-full h-10 flex flex-row justify-around items-center shadow-xl bg-gray-900 text-gray-100">
             <p>Home</p>
             <p>About Me</p>
             <p>Login / Register</p>
