b1ab4ddb08a06b95460dfa8822c9d6fcdc678516
Nicolas <nicopua7@gmail.com>
2023-08-17 02:14:40 -0300

    Logo and title added to APP

--- images-gallery/components/NavBar.tsx
+++ images-gallery/components/NavBar.tsx
@@ -1,9 +1,30 @@
+import Image from "next/image";
+
 const NavBar = () =>{
     return (
-        <nav className="inset-0 z-10 w-full h-10 flex flex-row justify-around items-center shadow-xl bg-gray-900 text-gray-100">
-            <p>Home</p>
-            <p>About Me</p>
-            <p>Login / Register</p>
+        <nav className="inset-0 z-10 w-full h-14 flex flex-row justify-around items-center shadow-xl bg-gray-900 text-gray-100">
+            <div className="pl-5 w-1/3 flex">
+                <Image 
+                    src="/images/onlylogo.png"
+                    alt="onlylogo"
+                    width={60}
+                    height={50}    
+                />
+                <Image
+                    className="pl-5"
+                    src="/images/onlytitle.png"
+                    alt="onlytitle"
+                    width={200}
+                    height={10}
+                />
+            </div>
+            <div className="bg-red-900 w-1/3 flex justify-around text-lg">
+                <p>Home</p>
+                <p>About Me</p>
+            </div>
+            <div className="bg-green-900 w-1/3">
+                <p>Login / Register</p>
+            </div>
         </nav>
     )
 }
