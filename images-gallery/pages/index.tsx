03cfc4780bb46c0ae60af9a76a363c5461b90abd
Nicolas <nicopua7@gmail.com>
2023-08-22 02:14:09 -0300

    Photo Cards added

--- images-gallery/pages/index.tsx
+++ images-gallery/pages/index.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image'
 import Layout from '@/components/Layout'
 import BackgroundHome from '@/components/BackgroundHome'
+import HomeRandomGallery from '@/components/Home/HomeRandomGallery'
+import { getUsers } from "@/utils/redux/actions";
+import { useEffect } from "react";
+import { useAppDispatch } from '@/utils/redux/hooks';
+
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+
+  useEffect(()=>{
+    dispatch(getUsers());
+  },[])
+
   return (
     <Layout title='Home | Images-Gallery' description='App de Imagenes'>
         <BackgroundHome>
           <div className='w-1/2 h-3/5 select-none flex flex-col justify-around'>
-            <h1 className='font-Coolvetica text-4xl w-3/4'>
+            <h1 className="font-bold text-4xl w-3/4">
               Busca tus imágenes gratis, libres de uso, rápido y descárgalo. 
               <br/>Postea las imágenes que desees.
             </h1>
-            <h1></h1>
             <div className='w-full h-fit text-black flex rounded-lg bg-white'>
-              <select className='font-bold pl-2 m-2 bg-gray-200 border-2 border-gray-400 w-1/6 rounded-lg focus:outline-none focus:bg-gray-200'>
-                <option>🖼️ Fotos</option>
-                <option>🎬 Videos</option>
+              <select className='font-bold pl-2 m-2 bg-gray-200 border-2 border-gray-400 w-1/6 rounded-lg focus:outline-none focus:bg-gray-300'>
+                <option className='bg-white'>🖼️ Fotos</option>
+                <option className='bg-white'>🎬 Videos</option>
               </select>
               <input 
                 className='mx-5 w-4/5 h-14 font-semibold text-xl focus:outline-none'
@@ -38,11 +48,9 @@ export default function Home() {
           </div>
         </BackgroundHome>
 
+      <HomeRandomGallery />
+      
       <div className='h-96'>
-        <h1>Holaa, estoy en el Home</h1>
-      </div>
-
-      <div>
         <h1>Holaa, estoy en el Home</h1>
       </div>
     </Layout>
