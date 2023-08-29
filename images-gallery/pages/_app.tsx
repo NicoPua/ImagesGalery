07bb2761d402d6b0f221edc462ff5c509545b921
Nicolas <nicopua7@gmail.com>
2023-08-28 21:22:36 -0300

    chackra-ui dependencie installed

--- images-gallery/pages/_app.tsx
+++ images-gallery/pages/_app.tsx
@@ -1,11 +1,23 @@
 import '@/styles/globals.css'
 import { Providers } from '@/utils/redux/providers'
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import chakraTheme from '@chakra-ui/theme'
+
+const { Spinner } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Spinner,
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
-      <Component {...pageProps} /> 
+      <ChakraBaseProvider theme={theme}>
+        <Component {...pageProps} />     
+      </ChakraBaseProvider>   
     </Providers>
   )
 }
