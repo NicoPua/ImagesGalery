6367694a364a5e942045ff1b28890fcb931b1fbe
Nicolas <nicopua7@gmail.com>
2023-08-30 03:40:49 -0300

    NavBar finished

--- images-gallery/components/NavBar.tsx
+++ images-gallery/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const NavBar = () =>{
     return (
         <nav className="inset-0 z-10 w-full h-14 flex flex-row justify-around items-center shadow-xl bg-gray-900 text-gray-100">
-            <div className="select-none pl-5 w-1/3 flex"> 
+            <div className="select-none pl-5 w-1/3 flex items-center"> 
                 <Link href="/">
                     <Image 
                         className="select-none"
@@ -24,12 +24,32 @@ const NavBar = () =>{
                     />
                 </Link>
             </div>
-            <div className="bg-red-900 w-1/3 flex justify-around text-lg">
-                <p>Home</p>
-                <p>About Me</p>
+            <div className="w-1/3 h-full flex text-lg">
+                <Link 
+                    className="hover:bg-white hover:text-black w-1/3 h-full flex justify-center items-center
+                    transition-all ease-in-out duration-200" 
+                    href="/">
+                    <p>Home</p>
+                </Link>
+                <Link 
+                    className="hover:bg-white hover:text-black w-1/3 h-full flex justify-center items-center
+                    transition-all ease-in-out duration-200"
+                    href="/">
+                    <p>Upload Image</p>
+                </Link>
+                <Link
+                    className="hover:bg-white hover:text-black w-1/3 h-full flex justify-center items-center
+                    transition-all ease-in-out duration-200"
+                    href="/aboutme">
+                    <p>About Me</p>
+                </Link>
             </div>
-            <div className="bg-green-900 w-1/3">
-                <p>Login / Register</p>
+            <div 
+                className="w-1/6 rounded-xl p-2 flex justify-center items-center
+                    bg-gray-700 border-2 border-white hover:bg-white hover:border-black hover:text-black
+                    transition-all ease-in-out duration-200
+                    ">
+                <p className="font-bold text-lg">Login / Register</p>
             </div>
         </nav>
     )
