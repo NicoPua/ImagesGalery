e01fac9fd87d864fba7dd62dd98070f2050da183
Nicolas <nicopua7@gmail.com>
2023-09-01 18:23:42 -0300

    validation function correction

--- images-gallery/aux-functions/validationPostData.ts
+++ images-gallery/aux-functions/validationPostData.ts
@@ -12,20 +12,16 @@ const validationPostData = ( { description, location, image } : BodyInformation 
         error.message = "La descripción es requerida.";
     }else if(!regexWithoutNumbers.test(description)){
         error.message = "La descripción no debe contener números.";
-    }else if(description.length <= 5 || description.length >= 100){
-        error.message = "La descripción debe contener entre 5 a 100 caracteres.";
+    }else if(description.length <= 5 || description.length >= 300){
+        error.message = "La descripción debe contener entre 5 a 300 caracteres.";
     }
 
     if(location.length === 0){
         error.message = "La location es requerida.";
     }else if(!regexOnlyLetters.test(location)){
         error.message = "La location debe contener sólo texto.";
-    }else if(description.length <= 2 || description.length >= 50){
+    }else if(location.length <= 2 || location.length >= 50){
         error.message = "La location debe contener entre 2 a 50 caracteres.";
-    }
-
-    if(image && !regexOnlyURL.test(image)){
-        error.message = "La imagen debe estar en formato URL.";
     }
 
     return error.message;
