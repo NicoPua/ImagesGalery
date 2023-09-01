f2f068ecda73cf1081a4a4138857e60c386b3b10
Nicolas <nicopua7@gmail.com>
2023-09-01 18:25:18 -0300

    cloudinary confi

--- images-gallery/utils/cloudinary/cloudinaryConfig.ts
+++ images-gallery/utils/cloudinary/cloudinaryConfig.ts
@@ -2,17 +2,20 @@ import {v2 as cloudinary} from 'cloudinary';
 
 const {CLOUD_NAME, CLOUD_KEY, CLOUD_SECRET} = process.env;
 
-const uptloadCl = async (img : string) => {
-
-    cloudinary.config({
+cloudinary.config({
     cloud_name: CLOUD_NAME,
     api_key: CLOUD_KEY,
     api_secret: CLOUD_SECRET
-    });
+});
 
-    const clImg = await cloudinary.uploader.upload(img)
-    return clImg.secure_url;
-
+const uploadCl = async (fileImg : any) => {
+    const clImage = await cloudinary.uploader.upload(fileImg);
+    return clImage.secure_url;
 }
 
-module.exports = { uptloadCl }
+const deleteCl = async (uploadedImage : any) => {
+    const deletedImage = await cloudinary.uploader.destroy(uploadedImage.public_id);
+    return deletedImage;
+}
+
+export { uploadCl, deleteCl }
