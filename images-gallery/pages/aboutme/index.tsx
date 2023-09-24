4694d8cd0184bde75fa064ed578d9995ee1b746a
Nicolas <nicopua7@gmail.com>
2023-09-24 14:34:21 -0300

    about me finished

--- images-gallery/pages/aboutme/index.tsx
+++ images-gallery/pages/aboutme/index.tsx
@@ -1,26 +1,34 @@
 import Image from 'next/image'
 import Layout from '@/components/Layout'
-import { Inter } from 'next/font/google'
+import SimpleSlider from '@/components/Carousel/Carousel';
 
-const inter = Inter({ subsets: ['latin'] })
+//import { Inter } from 'next/font/google'
+//const inter = Inter({ subsets: ['latin'] })
 
 export default function AboutMe() {
   return (
     <Layout title='About Me | PicsArt Gallery' description='Información acerca del creador de esta app.'>
-      <div className='bg-gray-100 pt-10 bg-red-400 w-full h-screen flex justify-center items-center'>
-        <div className=' w-3/4 h-3/4 bg-gray-900 text-gray-100 flex justify-around rounded-xl shadow-2xl'>
-          <div className='p-10'>
-            <h1 className='text-4xl pb-5'>💻Full Stack Web Developer</h1>
-            <h2 className='text-2xl pb-5'>Desarrollado por Gonzalo Nicolás Pua</h2>
+      <div className='bg-gray-100 pt-10 my-10 w-full h-screen flex flex-col justify-center items-center'>
+        <div className=' w-4/5 h-fit bg-gray-900 text-gray-100 flex justify-around rounded-xl shadow-2xl'>
+          <div className='p-10 flex flex-col w-1/2'>
+            <h1 className='font-bold text-4xl pb-5'>💻Full Stack Web Developer</h1>
+            <h2 className='font-semibold text-2xl pb-5'>Desarrollado por Gonzalo Nicolás Pua</h2>
+            <p>PicsArt es una aplicación web con el fin de poder almacenar imágenes y descargar las mismas con libertad de uso con el consentimiento del usuario. PicsArt es una aplicación web destinada a fines educativos.</p>
             <p>Esta aplicación fue desarrollada con tecnologías tales como: TypeScript, React JS, Next JS, Tailwind CSS, MongoDB & Mongoose. </p>
+            <div className='w-full flex justify-center mt-5 p-3'>
+              <SimpleSlider />
+            </div>
           </div>
 
-          <Image 
-            className='rounded-xl shadow-xl'
-            src='/images/cuttedprofilepic.jpg'
-            width={550} 
-            height={500} 
-            alt='profilepic'/>
+          <div className='w-full flex justify-end'>
+            <Image 
+              className='rounded-xl shadow-xl'
+              src='/images/cuttedprofilepic.jpg'
+              width={550} 
+              height={500} 
+              alt='profilepic'
+            />
+          </div>
         </div>
       </div>
     </Layout>
